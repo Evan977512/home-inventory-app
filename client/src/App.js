019a11343ade2ac13b91971376c4c876d11a27ae
EvanKim <youngchan.kim@edu.sait.ca>
2023-02-26 02:40:45 -0700

    bug fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,87 +1,66 @@
-import Topbar from "./components/topbar/Topbar";
-import Settings from "./pages/settings/Settings";
-import Signin from "./pages/signin/Signin";
-import Signup from "./pages/signup/Signup";
-import Home from "./pages/home/Home";
-import About from "./pages/about/About";
-import Inventory from "./pages/inventory/Inventory";
-import Single from "./pages/single/Single";
-import ManageCategory from "./pages/admin/manageCategory/ManageCategory";
-import ManageUser from "./pages/admin/manageUser/ManageUser";
-import AddItem from "./components/addItem/AddItem";
-import AddUser from "./components/addUser/AddUser";
-import EditUser from "./components/editUser/EditUser";
+import Topbar from './components/topbar/Topbar';
+import Settings from './pages/settings/Settings';
+import Signin from './pages/signin/Signin';
+import Signup from './pages/signup/Signup';
+import Home from './pages/home/Home';
+import About from './pages/about/About';
+import Inventory from './pages/inventory/Inventory';
+import Single from './pages/single/Single';
+import ManageCategory from './pages/admin/manageCategory/manageCategory';
+import ManageUser from './pages/admin/manageUser/ManageUser';
+import AddItem from './components/addItem/AddItem';
+import AddUser from './components/addUser/AddUser';
+import EditUser from './components/editUser/EditUser';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // React-Router는 신규 페이지를 불러오지 않는 상황에서
 // 각각의 url에 따라 선택된 데이터를 하나의 페이지에서 렌더링 해주는 라이브러리
 
-import { useContext } from "react";
-import { Context } from "./context/Context";
+import { useContext } from 'react';
+import { Context } from './context/Context';
 
 //global 지정
-let systemAdmin = "";
-let companyAdmin = "";
-let regularUser = "";
+let systemAdmin = '';
+let companyAdmin = '';
+let regularUser = '';
 
 function App() {
-  const { user } = useContext(Context);
-  //context를 이용하면 단계마다 일일이 props를 넘겨주지 않고도 컴포넌트 트리 전체에 데이터를 제공할 수 있음
-  if (user) {
-    if (user.role === 1){
-      systemAdmin = user;
-      // console.log("systemAdmin: " + JSON.stringify(systemAdmin));
-    } else if (user.role === 3) {
-      companyAdmin = user; 
-      // console.log("companyAdmin: " + JSON.stringify(companyAdmin));
-    } else {
-      regularUser = user;
-      // console.log("regularUser: " + JSON.stringify(regularUser));
+    const { user } = useContext(Context);
+    //context를 이용하면 단계마다 일일이 props를 넘겨주지 않고도 컴포넌트 트리 전체에 데이터를 제공할 수 있음
+    if (user) {
+        if (user.role === 1) {
+            systemAdmin = user;
+            // console.log("systemAdmin: " + JSON.stringify(systemAdmin));
+        } else if (user.role === 3) {
+            companyAdmin = user;
+            // console.log("companyAdmin: " + JSON.stringify(companyAdmin));
+        } else {
+            regularUser = user;
+            // console.log("regularUser: " + JSON.stringify(regularUser));
+        }
     }
-  }
 
-  return (
-    <BrowserRouter>
-      <Topbar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/inventory"
-          element={user ? <Inventory /> : <Signup />}
-        />
-        <Route path="/signup" element={user ? <Home /> : <Signup />} />
-        <Route path="/signin" element={user ? <Home /> : <Signin />} />
-        <Route path="/item/:itemId" element={user? <Single /> : <Signin />} />
-        {/* <Route path="/inventory/:categoryName" element={user? <Inventory /> : <Signin />} /> */}
-        <Route
-          path="/addItem"
-          element={user ? <AddItem /> : <Signin />}
-        />
-        <Route
-          path="/settings"
-          element={user ? <Settings /> : <Signup />}
-        />
-        {/* didn't set admin page yet */}
-        <Route
-          path="/admin/manageCategory"
-          element={systemAdmin || companyAdmin ? <ManageCategory /> : <Signin />}
-        />
-        <Route
-          path="/admin/manageUser"
-          element={systemAdmin || companyAdmin  ? <ManageUser /> : <Signin />}
-        />
-        <Route
-          path="/admin/manageUser/addUser"
-          element={systemAdmin || companyAdmin  ? <AddUser /> : <Signin />}
-        />
-        <Route
-          path="/admin/manageUser/editUser/:userId"
-          element={systemAdmin || companyAdmin  ? <EditUser /> : <Signin />}
-        />
-      </Routes>
-    </BrowserRouter> //need to be one div
-  );
+    return (
+        <BrowserRouter>
+            <Topbar />
+            <Routes>
+                <Route exact path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/inventory" element={user ? <Inventory /> : <Signup />} />
+                <Route path="/signup" element={user ? <Home /> : <Signup />} />
+                <Route path="/signin" element={user ? <Home /> : <Signin />} />
+                <Route path="/item/:itemId" element={user ? <Single /> : <Signin />} />
+                {/* <Route path="/inventory/:categoryName" element={user? <Inventory /> : <Signin />} /> */}
+                <Route path="/addItem" element={user ? <AddItem /> : <Signin />} />
+                <Route path="/settings" element={user ? <Settings /> : <Signup />} />
+                {/* didn't set admin page yet */}
+                <Route path="/admin/manageCategory" element={systemAdmin || companyAdmin ? <ManageCategory /> : <Signin />} />
+                <Route path="/admin/manageUser" element={systemAdmin || companyAdmin ? <ManageUser /> : <Signin />} />
+                <Route path="/admin/manageUser/addUser" element={systemAdmin || companyAdmin ? <AddUser /> : <Signin />} />
+                <Route path="/admin/manageUser/editUser/:userId" element={systemAdmin || companyAdmin ? <EditUser /> : <Signin />} />
+            </Routes>
+        </BrowserRouter> //need to be one div
+    );
 }
 
 export default App;
--- client/src/pages/admin/manageCategory/manageCategory.jsx
+++ client/src/pages/admin/manageCategory/manageCategory.jsx
@@ -1,208 +1,192 @@
-import React from "react";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 //import { useLocation } from "react-router";
-import "./manageCategory.css";
-import Categories from "../../../components/categories/Categories";
-import AddCategory from "../../../components/addCategory/AddCategory";
-import Swal from "sweetalert2";
+import './manageCategory.css';
+import Categories from '../../../components/categories/Categories';
+import AddCategory from '../../../components/addCategory/AddCategory';
+import Swal from 'sweetalert2';
 
 function ManageCategory() {
-  const [categories, setCategories] = useState([]);
-  const [addCategoryClicked, setAddCategoryClicked] = useState(false);
+    const [categories, setCategories] = useState([]);
+    const [addCategoryClicked, setAddCategoryClicked] = useState(false);
 
-  const addCategory = () => {
-    setAddCategoryClicked(true);
-  };
+    const addCategory = () => {
+        setAddCategoryClicked(true);
+    };
 
-  const handleEdit = async (category, name) => {
-    const editedCategory = { name };
-    // console.log("ManageCategory: " + name);
-    try {
-      const res = await axios.put(
-        `http://localhost:3000/homeserver/api/categories/${category._id}`,
-        editedCategory
-      );
-      if (res.status === 200) {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener("mouseenter", Swal.stopTimer);
-            toast.addEventListener("mouseleave", Swal.resumeTimer);
-          },
-        });
+    const handleEdit = async (category, name) => {
+        const editedCategory = { name };
+        // console.log("ManageCategory: " + name);
+        try {
+            const res = await axios.put(`http://localhost:3000/homeserver/api/categories/${category._id}`, editedCategory);
+            if (res.status === 200) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer);
+                        toast.addEventListener('mouseleave', Swal.resumeTimer);
+                    },
+                });
 
-        Toast.fire({
-          icon: "success",
-          title: "Category is edited in successfuly",
-        });
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Category is edited in successfuly',
+                });
+                fetchCategories().then((data) => {
+                    setCategories(data);
+                });
+            }
+        } catch (err) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer);
+                    toast.addEventListener('mouseleave', Swal.resumeTimer);
+                },
+            });
+
+            Toast.fire({
+                icon: 'error',
+                title: 'Category is not edited',
+            });
+        }
+    };
+
+    const handleDelete = async (category) => {
+        try {
+            const res = await axios.delete(`http://localhost:3000/homeserver/api/categories/${category._id}`);
+            if (res.status === 200) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer);
+                        toast.addEventListener('mouseleave', Swal.resumeTimer);
+                    },
+                });
+
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Category is deleted in successfuly',
+                });
+                fetchCategories().then((data) => {
+                    setCategories(data);
+                });
+            }
+        } catch (err) {
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer);
+                    toast.addEventListener('mouseleave', Swal.resumeTimer);
+                },
+            });
+
+            Toast.fire({
+                icon: 'error',
+                title: 'Category is not deleted',
+            });
+        }
+    };
+
+    const handleSubmit = async (name) => {
+        const newCategory = { name };
+
+        try {
+            const res = await axios.post('http://localhost:3000/homeserver/api/categories', newCategory);
+            if (res.status === 200) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer);
+                        toast.addEventListener('mouseleave', Swal.resumeTimer);
+                    },
+                });
+
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Category is added in successfuly',
+                });
+                window.scrollTo(0, document.body.scrollHeight);
+
+                fetchCategories().then((data) => {
+                    setCategories(data);
+                });
+            } else {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer);
+                        toast.addEventListener('mouseleave', Swal.resumeTimer);
+                    },
+                });
+
+                Toast.fire({
+                    icon: 'success',
+                    title: 'Category is not added',
+                });
+            }
+        } catch (err) {
+            // console.log(err);
+        }
+    };
+
+    const fetchCategories = async () => {
+        const res = await axios.get('http://localhost:3000/homeserver/api/categories/');
+        return res.data;
+    };
+
+    useEffect(() => {
         fetchCategories().then((data) => {
-          setCategories(data);
+            setCategories(data);
         });
-      }
-    } catch (err) {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener("mouseenter", Swal.stopTimer);
-          toast.addEventListener("mouseleave", Swal.resumeTimer);
-        },
-      });
+    }, []);
 
-      Toast.fire({
-        icon: "error",
-        title: "Category is not edited",
-      });
-    }
-  };
-
-  const handleDelete = async (category) => {
-    try {
-      const res = await axios.delete(
-        `http://localhost:3000/homeserver/api/categories/${category._id}`
-      );
-      if (res.status === 200) {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener("mouseenter", Swal.stopTimer);
-            toast.addEventListener("mouseleave", Swal.resumeTimer);
-          },
-        });
-
-        Toast.fire({
-          icon: "success",
-          title: "Category is deleted in successfuly",
-        });
-        fetchCategories().then((data) => {
-          setCategories(data);
-        });
-      }
-    } catch (err) {
-      const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener("mouseenter", Swal.stopTimer);
-          toast.addEventListener("mouseleave", Swal.resumeTimer);
-        },
-      });
-
-      Toast.fire({
-        icon: "error",
-        title: "Category is not deleted",
-      });
-    }
-  };
-
-  const handleSubmit = async (name) => {
-    const newCategory = { name };
-
-    try {
-      const res = await axios.post(
-        "http://localhost:3000/homeserver/api/categories",
-        newCategory
-      );
-      if (res.status === 200) {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener("mouseenter", Swal.stopTimer);
-            toast.addEventListener("mouseleave", Swal.resumeTimer);
-          },
-        });
-
-        Toast.fire({
-          icon: "success",
-          title: "Category is added in successfuly",
-        });
-        window.scrollTo(0, document.body.scrollHeight);
-
-        fetchCategories().then((data) => {
-          setCategories(data);
-        });
-      } else {
-        const Toast = Swal.mixin({
-          toast: true,
-          position: "top-end",
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.addEventListener("mouseenter", Swal.stopTimer);
-            toast.addEventListener("mouseleave", Swal.resumeTimer);
-          },
-        });
-
-        Toast.fire({
-          icon: "success",
-          title: "Category is not added",
-        });
-      }
-    } catch (err) {
-      // console.log(err);
-    }
-  };
-
-  const fetchCategories = async () => {
-    const res = await axios.get(
-      "http://localhost:3000/homeserver/api/categories/"
+    return (
+        <>
+            {/* {console.log(categories)} */}
+            <div className="home">
+                <div className="manageCategory_top">
+                    <h1 className="manageCategory_h1">Manage Categories</h1>
+                    <div className="manageCategory_btn_div">
+                        <button className="manageCategory_btn_add" onClick={addCategory}>
+                            + Add Category
+                        </button>
+                    </div>
+                </div>
+                {addCategoryClicked === true ? <AddCategory handleSubmit={handleSubmit} /> : null}
+                {categories ? (
+                    <Categories categories={categories} handleEdit={handleEdit} handleDelete={handleDelete} />
+                ) : (
+                    <span>Category is empty</span>
+                )}
+            </div>
+        </>
     );
-    return res.data;
-  };
-
-  useEffect(() => {
-    fetchCategories().then((data) => {
-      setCategories(data);
-    });
-  }, []);
-
-  return (
-    <>
-      {/* {console.log(categories)} */}
-      <div className="home">
-        <div className="manageCategory_top">
-          <h1 className="manageCategory_h1">Manage Categories</h1>
-          <div className="manageCategory_btn_div">
-            <button className="manageCategory_btn_add" onClick={addCategory}>
-              + Add Category
-            </button>
-          </div>
-        </div>
-        {addCategoryClicked === true ? (
-          <AddCategory handleSubmit={handleSubmit} />
-        ) : null}
-        {categories ? (
-          <Categories
-            categories={categories}
-            handleEdit={handleEdit}
-            handleDelete={handleDelete}
-          />
-        ) : (
-          <span>Category is empty</span>
-        )}
-      </div>
-    </>
-  );
 }
 
 export default ManageCategory;
